c1d47837f82b890d2562f557afcae76c7e3e161c
animesh <animesh317kumar@gmail.com>
2024-05-11 22:31:46 +0530

    Added projectList

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,13 @@
+import Proj1 from "../assets/proj1.jpg";
+import Proj3 from "../assets/proj3.webp";
+
+export const projectList = [
+    {
+        name: "Dijkstra's Algorithm Visualizer",
+        image: Proj1
+    },
+    {
+        name: "Spotify Clone",
+        image: Proj3
+    },
+];
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import { ProjectItem } from '../components/ProjectItem';
-import Proj1 from "../assets/proj1.jpg";
-import Proj3 from "../assets/proj3.webp";
 import "../styles/Projects.css"
+import { projectList } from '../helpers/ProjectList';
 
 export const Projects = () => {
   return (
     <div className='projects'>
       <h1>My Personal Projects</h1>
       <div className='projectList'>
-        <ProjectItem name="Social Media Website" image={Proj1} />
-        <ProjectItem name="Spotiify Clone" image={Proj3} />
+        {projectList.map((project) => {
+          return <ProjectItem name={project.name} image={project.image} />
+        })}
       </div>
     </div>
   )
